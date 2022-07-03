f0bd3d20c8316030b80a60700e35d50e2495e89c
Hyeonm1339 <jhjimess@naver.com>
2022-07-03 22:24:29 +0900

    섹션5까지 완료

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,19 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ExpenseForm from './ExpenseForm'
 import './NewExpense.css'
 
 const NewExpense = (props) => {
+  const [isEditing, setIsEditing] = useState(false)
   const saveExpenseDataHandler = (enteredExpenseData) => {
     const expenseData = {
       ...enteredExpenseData,
       id: Math.random().toString(),
     }
     props.onAddExpense(expenseData)
+    setIsEditing(false)
+  }
+
+  const startEditingHandler = () => {
+    setIsEditing(true)
+  }
+
+  const stopEditingHandle = () => {
+    setIsEditing(false)
   }
 
   return (
     <div className="new-expense">
-      <ExpenseForm onSaveExpenseDate={saveExpenseDataHandler} />
+      {!isEditing && (
+        <button onClick={startEditingHandler}>Add New Expense</button>
+      )}
+      {isEditing && (
+        <ExpenseForm
+          onSaveExpenseDate={saveExpenseDataHandler}
+          onCancle={stopEditingHandle}
+        />
+      )}
     </div>
   )
 }
